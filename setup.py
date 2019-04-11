156108ee588f6bc27c90a45282aa5c7f3a3183cf
Danilo Horta <danilo.horta@gmail.com>
2019-04-11 11:30:41 +0100

    setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="struct-lmm2",
+    maintainer="Anna",
+    description="Nice package",
+    author="Anna",
+    license="BSD",
+    python_requires=">=3.6",
+    install_requires=["numpy>=1.6", "limix", "numpy-sugar"],
+)
